ce6fa1cacfefa806bf84962735d9a66e1b400ced
Vincent <vkl2109@columbia.edu>
2023-01-20 14:25:11 -0500

    Added simple login image page with login button

--- client/App.js
+++ client/App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import LoginButton from './components/LoginButton';
+import HomeImage from './components/HomeImage';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Paint Swap App</Text>
+      <View style={styles.imageContainer}>
+        <HomeImage placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <LoginButton label="Login" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
